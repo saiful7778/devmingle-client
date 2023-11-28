f5815859043449fe593c1a18141f920ea5d54a75
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2023-11-28 22:12:27 +0600

    add all initial files

---
+++ src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import { Button } from "keep-react";
+
+const Home = () => {
+  return (
+    <Button
+      className="active:focus:scale-95 duration-100"
+      size="sm"
+      type="default"
+      typeof="submit"
+    >
+      Click
+    </Button>
+  );
+};
+
+export default Home;
