afed85a0613e0f9573c7b941b09a0dd5475bac7d
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-04-23 21:32:32 +0600

    add error data show

--- src/utility/errorStatus.js
+++ src/utility/errorStatus.js
@@ -3,24 +3,21 @@ import Swal from "sweetalert2";
 const errorStatus = (errorCode) => {
   switch (errorCode.code) {
     case "auth/email-already-in-use":
-      console.error(errorCode.code);
       Swal.fire({
         icon: "error",
         text: "This email is already exist! please login",
       });
       break;
     case "auth/invalid-credential":
-      console.error(errorCode.code);
       Swal.fire({
         icon: "error",
         text: "Email or Password doesn't match",
       });
       break;
     default:
-      console.error(errorCode.code);
       Swal.fire({
         icon: "error",
-        text: errorCode.code,
+        text: errorCode,
       });
       break;
   }
