80e5beef811e83f4e0e9de344d39e8a64c05b72a
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-04-24 00:23:39 +0600

    add more condition

--- src/utility/errorStatus.js
+++ src/utility/errorStatus.js
@@ -5,7 +5,19 @@ const errorStatus = (errorCode) => {
     case "auth/email-already-in-use":
       Swal.fire({
         icon: "error",
+        text: "This email is already in use! please login",
+      });
+      break;
+    case "auth/email-already-exists":
+      Swal.fire({
+        icon: "error",
         text: "This email is already exist! please login",
+      });
+      break;
+    case "auth/insufficient-permission":
+      Swal.fire({
+        icon: "error",
+        text: "Permission is declined. Please retry.",
       });
       break;
     case "auth/invalid-credential":
