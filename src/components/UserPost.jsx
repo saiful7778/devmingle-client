cc64eb1d8e0ecc97ae86177e5b242750a9c7fa70
Saiful Islam <saiful.islam.rafi.88@gmail.com>
2024-04-24 21:21:43 +0600

    update async and trycatch

--- src/components/UserPost.jsx
+++ src/components/UserPost.jsx
@@ -9,24 +9,26 @@ import { Link } from "react-router-dom";
 
 const UserPost = ({ inputData, reFatch }) => {
   const {
-    _id,
+    id,
     title,
     voteCount: { upVote, downVote },
-    comment: { count },
+    commentCount,
   } = inputData;
-  const axiosSecure = useAxiosSecure();
-  const { user } = useAuth();
 
-  const handleDelete = () => {
-    Swal.fire({
-      icon: "warning",
-      title: "Are you sure?",
-      text: `delete "${title}"`,
-      showCancelButton: true,
-      confirmButtonColor: "#3085d6",
-      cancelButtonColor: "#d33",
-      confirmButtonText: "Yes, delete it!",
-    }).then(({ isConfirmed }) => {
+  const axiosSecure = useAxiosSecure();
+  const { user, userData, token } = useAuth();
+
+  const handleDelete = async () => {
+    try {
+      const { isConfirmed } = await Swal.fire({
+        icon: "warning",
+        title: "Are you sure?",
+        text: `delete "${title}"`,
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!",
+      });
       if (isConfirmed) {
         Swal.fire({
           title: "Loading....",
@@ -35,53 +37,55 @@ const UserPost = ({ inputData, reFatch }) => {
           },
           showConfirmButton: false,
         });
-        axiosSecure
-          .delete(`/post/${_id}`, {
-            params: { email: user.email, uid: user.uid },
-          })
-          .then(({ data }) => {
-            if (data.result.deletedCount === 1) {
-              Swal.fire({
-                title: "Deleted!",
-                text: `"${title}" has been deleted.`,
-                icon: "success",
-              });
-              reFatch();
-            } else {
-              Swal.fire({
-                title: "Delete incomplate!",
-                text: `"${title}" not deleted.`,
-                icon: "error",
-              });
-              reFatch();
-            }
-          })
-          .catch((err) => {
-            console.error(err);
-            Swal.fire({
-              icon: "error",
-              text: err,
-            });
+        const { data } = await axiosSecure.delete(`/post/delete/${id}`, {
+          params: { email: user.email, userId: userData._id },
+          headers: { Authorization: `Bearer ${token}` },
+        });
+        if (!data.success) {
+          throw new Error("Something went wrong");
+        }
+        if (data?.data?.deletedCount === 1) {
+          Swal.fire({
+            icon: "success",
+            title: "Deleted!",
+            text: `"${title}" has been deleted.`,
           });
+          reFatch();
+        } else {
+          Swal.fire({
+            title: "Delete incomplate!",
+            text: `"${title}" not deleted.`,
+            icon: "error",
+          });
+          reFatch();
+        }
       }
-    });
+    } catch (err) {
+      Swal.fire({
+        icon: "error",
+        text: err,
+      });
+    }
   };
 
   return (
     <div className="flex overflow-hidden justify-between gap-2 p-2 rounded-lg border border-blue-600 bg-white">
       <div className="flex flex-col">
-        <Link to={`/post/${_id}`}>
+        <Link to={`/post/${id}`}>
           <h3 className="flex-1 leading-5 md:text-xl font-semibold capitalize hover:underline max-h-16 overflow-hidden">
             {title}
           </h3>
         </Link>
-        <div className="my-2">Comments: {count}</div>
+        <div className="my-2">Comments: {commentCount}</div>
         <div className="flex gap-2 items-center">
-          <Link to={`/dashboard/comments/${_id}`}>
-            <Button size="sm" className="btn" type="primary">
-              Comment
-            </Button>
-          </Link>
+          <Button
+            href={`/dashboard/comments/${id}`}
+            size="sm"
+            className="btn"
+            type="primary"
+          >
+            Comment
+          </Button>
           <Button
             onClick={handleDelete}
             size="sm"
